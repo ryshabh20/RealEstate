ab2620d606451d393903948f07b975e04aabfad1
Rishabh <rishabdahiya98@gmail.com>
2023-11-20 23:13:19 +0530

    add preisntallation

--- api/index.js
+++ api/index.js
@@ -5,8 +5,10 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listRouter from "./routes/listing.route.js";
+import path from "path";
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
 const PORT = 3000;
 mongoose
   .connect(process.env.MONGO_URL)
@@ -21,6 +23,10 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listRouter);
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
