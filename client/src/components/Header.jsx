96f8fc2d6dd0c5507c26548fa643bae0b3bd692c
RishabhSingh <60999728+ryshabh20@users.noreply.github.com>
2023-11-21 10:28:48 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,8 +27,8 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-400">Second</span>
-            <span className="text-white">Floor</span>
+            <span className="text-slate-400">Sky</span>
+            <span className="text-white">Scape</span>
           </h1>
         </Link>
         <form
